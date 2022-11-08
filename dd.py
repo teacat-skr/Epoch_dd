1122e5417add1d7fbaf95e4558e500809fcfcebd
teacat-skr <railcore03@gmail.com>
2022-11-08 06:50:13 +0000

    update

--- dd.py
+++ dd.py
@@ -9,152 +9,106 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 import math
 from model import resnet18k 
+from data import tin 
+import torchvision.models as models
+
 
 import argparse
 import random
 import matplotlib.pyplot as plt
 import csv 
 import warnings
-    
+
+def get_model(args):
+    """モデルの読み込み
+
+    Arguments:
+        args: 
+    Returns:
+        model
+    """
+    if args.model == "resnet18k":
+        assert args.model_width is not None, "please check k value"
+        model = resnet18k.make_resnet18k(k=args.model_width, num_classes=args.num_classes)
+        args.model_fullname = "SR_resnet18k-{}".format(args.model_width)
+    elif args.model == "resnet18":
+        if args.pretrained == False:
+            # model
+            model = models.resnet18(pretrained=False, num_classes=args.num_classes)
+            args.model_fullname = "SR_resnet18" # SR means scratch
+        else: ## 事前学習モデルを利用する場合，一度重みを読み込んで，全結合層だけ初期化する
+            model = models.resnet18(pretrained=True)
+            in_features = model.fc.in_features
+            model.fc = nn.Linear(in_features=in_features, out_features=args.num_classes, bias=True)
+            args.model_fullname = "IN_resnet18"
+    elif args.model == "resnet50":
+        if args.pretrained == False:
+            # model
+            model = models.resnet50(pretrained=False, num_classes=args.num_classes)
+            args.model_fullname = "SR_resnet50" # SR means scratch
+        else: ## 事前学習モデルを利用する場合，一度重みを読み込んで，全結合層だけ初期化する
+            model = models.resnet50(pretrained=True)
+            in_features = model.fc.in_features
+            model.fc = nn.Linear(in_features=in_features, out_features=args.num_classes, bias=True)
+            args.model_fullname = "IN_resnet50"
+    return model
+
+def get_dataset_train(args, transform):
+    if args.dataset == "cifar10":
+        return torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    elif args.dataset == "cifar100":
+        return torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+    elif args.dataset == "tinyImageNet":
+        return torchvision.datasets.ImageFolder(root='./data/tiny-imagenet-200/train', transform=transform)
+        
+def get_dataset_test(args, transform):
+    if args.dataset == "cifar10":
+        return torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    elif args.dataset == "cifar100":
+        return torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+    elif args.dataset == "tinyImageNet":
+        return torchvision.datasets.ImageFolder(root='./data/tiny-imagenet-200/val', transform=transform)
+
+def get_imagesize(args):
+    if args.dataset == "cifar10":
+        return 32
+    elif args.dataset == "cifar100":
+        return 32
+    elif args.dataset == "tinyImageNet":
+        return 64
+
+def get_num_classes(args):
+    if args.dataset == "cifar10":
+        return 10
+    elif args.dataset == "cifar100":
+        return 100
+    elif args.dataset == "tinyImageNet":
+        return 200
+
+def fix_seed(seed=42):
+    # random
+    random.seed(seed)
+    # Numpy
+    # np.random.seed(seed)
+    # Pytorch
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    # Tensorflow
+    # tf.random.set_seed(seed)
+
 def main():
     args = parse_args()
-    #step数指定
-    grad_steps = args.grad_steps
-    label_noise_rate = args.label_noise_rate
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    #インスタンス変数にアクセスしてラベルの張替え
-    for i in range(len(train_set.targets)):
-        if(random.randint(0, 9999) < int(label_noise_rate * 10000)):
-            train_set.targets[i] += random.randint(1, 9)
-            train_set.targets[i] %= 10
-    
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
-    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, num_workers=2)
-    class_names = ('plane', 'car', 'bird', 'cat', 'dog', 'frog', 'ship', 'truck')
-    model = resnet18k.make_resnet18k(k=args.model_width)
-    model = model.to(device)
-    
-    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-    def func(steps):
-        if steps < 512:
-            return 1
-        else:
-            return 1.0 / math.sqrt(math.floor(steps / 512.0))
-    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = func)
-    x1 = range(grad_steps + 1)
-    x2 = []
-    trerr = []
-    teerr = []
-    trloss = []
-    teloss = []
-    x2.append(0)
-    train_data_save = []
-    train_data_save.append(["step", "error", "loss"])
-    train_acc, train_loss = test(model, device, test_loader, criterion)
-    #test()を使ってtrainデータのerrorとlossの初期値を取得
-    train_data_save.append([0, 1.0 - train_acc, train_loss])
-    trerr.append(1.0 - train_acc)
-    trloss.append(train_loss)
-
-    test_data_save = []
-    test_data_save.append(["step", "error", "loss"])
-    test_acc, test_loss = test(model, device, test_loader, criterion)
-    #test()を使ってtestデータのerrorとlossの初期値を取得
-    test_data_save.append([0, 1.0 - test_acc, test_loss])
-    teerr.append(1.0 - test_acc)
-    teloss.append(test_loss)
-
-    count = 0
-    while(grad_steps != count):
-        running_loss = 0.0
-        model.train()
-        for batich_idx, (inputs, targets) in enumerate(train_loader):
-            if(grad_steps == count):
-                break
-            output_list = []
-            target_list = []
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            output_list += [int(o.argmax()) for o in outputs]
-            target_list += [int(t) for t in targets]
-            running_loss += loss.item()
-
-            train_acc, train_loss = calc_score(target_list, output_list, running_loss, train_loader)
-            if count % 1000 == 0 and count != 0:
-                stdout_temp = 'step: {:>3}/{:<3}, train acc:{:<8}, train loss: {:<8}'
-                print(stdout_temp.format(count, grad_steps, train_acc, train_loss))
-            trerr.append(1.0 - train_acc)
-            trloss.append(loss.item())
-            count += 1
-            scheduler.step()
-            train_data_save.append([count, 1.0 - train_acc, train_loss])
-        test_acc, test_loss = test(model, device, test_loader, criterion)
-        stdout_temp = 'step: {:>3}/{:<3}, test acc:{:<8}, test loss: {:<8}'
-        print(stdout_temp.format(count, grad_steps, test_acc, test_loss))
-        teerr.append(1.0 - test_acc)
-        teloss.append(test_loss)
-        test_data_save.append([count, 1.0 - train_acc, train_loss])
-        x2.append(count)
-    
-    with open('./csv/resnet18_' + str(args.model_width) + '-cifar10-train.csv','w') as file:
-        writer = csv.writer(file)
-        writer.writerows(train_data_save)
-
-    with open('./csv/resnet18_' + str(args.model_width) + '-cifar10-test.csv','w') as file:
-        writer = csv.writer(file)
-        writer.writerows(test_data_save)
-    
-    #errorのグラフ化
-    plt.title("ResNet18*" + str(args.model_width) + " trained by Cifar10")
-    plt.xlim(0, grad_steps * 1.2)
-    plt.ylim(0, 1)
-    plt.xlabel("Steps")
-    plt.ylabel("Erorr")
-    plt.plot(x1, trerr, label = 'train', linewidth=0.5)
-    plt.plot(x2, teerr, marker='.', label='test')
-    plt.legend(loc='upper right')
-    plt.savefig("./output/ResNet18_" + str(args.model_width) + "TrainedByCifar10.png")
-
-    plt.close()
-
-    #lossのグラフ化
-    plt.title("ResNet18*" + str(args.model_width) + " trained by Cifar10")
-    plt.xlim(0, grad_steps * 1.2)
-    plt.ylim(0, max(max(trloss) * 1.2, max(teloss) * 1.2))
-    plt.xlabel("Steps")
-    plt.ylabel("Loss")
-    plt.plot(x1, trloss, label = 'train', linewidth=0.5)
-    plt.plot(x2, teloss, marker='.', label='test')
-    plt.legend(loc='upper right')
-    plt.savefig("./output/loss-ResNet18_" + str(args.model_width) + "TrainedByCifar10.png")
-
-def sub():
-    args = parse_args()
+    fix_seed(42)
     #epoch数指定
-    epoch = args.grad_steps
+    epoch = args.epoch
     label_noise_rate = args.label_noise_rate
+    args.num_classes = get_num_classes(args)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    imageSize = get_imagesize(args)
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
+        transforms.RandomCrop(imageSize, padding=imageSize // 8),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -163,46 +117,52 @@ def sub():
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
-    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    train_set = get_dataset_train(args, transform_train)
+
     #インスタンス変数にアクセスしてラベルの張替え
     for i in range(len(train_set.targets)):
         if(random.randint(0, 9999) < int(label_noise_rate * 10000)):
-            train_set.targets[i] += random.randint(1, 9)
-            train_set.targets[i] %= 10
+            train_set.targets[i] += random.randint(1, args.num_classes - 1)
+            train_set.targets[i] %= args.num_classes
     
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=2)
-    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
+    test_set = get_dataset_test(args, transform_test)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=True, num_workers=2)
-    class_names = ('plane', 'car', 'bird', 'cat', 'dog', 'frog', 'ship', 'truck')
-    model = resnet18k.make_resnet18k(k=args.model_width)
-    model = model.to(device)
+    # CIFAR10のクラス
+    class_names = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
+    model = get_model(args)
+    model = model.to(device)
+    print(args.model_fullname)
+
+    # if device == 'cuda':
+        # model = torch.nn.DataParallel(model)
+
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
     x1 = []
     x2 = range(epoch + 1)
-    trerr = []
-    teerr = []
-    trloss = []
-    teloss = []
-    train_data_save = []
-
     x1.append(0)
-    train_data_save.append(["epoch", "error", "loss"])
-    train_acc, train_loss = test(model, device, test_loader, criterion)
-    #test()を使ってtrainデータのerrorとlossの初期値を取得
-    train_data_save.append([0, 1.0 - train_acc, train_loss])
-    trerr.append(1.0 - train_acc)
-    trloss.append(train_loss)
 
-    test_data_save = []
-    test_data_save.append(["epoch", "error", "loss"])
+    #----------#
+    ### csv 作成
+    #----------#
+    # 初期値取得
+    
+    train_acc, train_loss = test(model, device, test_loader, criterion) # 初期値取得
     test_acc, test_loss = test(model, device, test_loader, criterion)
-    #test()を使ってtestデータのerrorとlossの初期値を取得
-    test_data_save.append([0, 1.0 - test_acc, test_loss])
-    teerr.append(1.0 - test_acc)
-    teloss.append(test_loss)
-
+    # train
+    with open('./csv/{}_{}_{}epochs_ln{}pc_train.csv'.format(args.model_fullname, args.dataset, args.epoch, int(label_noise_rate * 100)),'w') as file:
+        file.write("epoch,error,loss\n")
+        file.write(f"0,{1.0 - train_acc},{train_loss}" + "\n")
+    # test
+    with open('./csv/{}_{}_{}epochs_ln{}pc_test.csv'.format(args.model_fullname, args.dataset, args.epoch, int(label_noise_rate * 100)),'w') as file:
+        file.write("epoch,error,loss\n")
+        file.write(f"0,{1.0 - test_acc},{test_loss}" + "\n")
+    
+    ## ----------------#
+    ## training #
+    # ----------------#
     for epoch in range(epoch):
         # Train and test a model.
         model.train()
@@ -227,56 +187,40 @@ def sub():
 
             train_acc, train_loss = calc_score(target_list, output_list, running_loss, train_loader)
             x1.append(xpoint)
-            trerr.append(1.0 - train_acc)
-            trloss.append(loss.item())
-            train_data_save.append([xpoint, 1.0 - train_acc, loss.item()])
+            ## csv writer
+            with open('./csv/{}_{}_{}epochs_ln{}pc_train.csv'.format(args.model_fullname, args.dataset, args.epoch, int(label_noise_rate * 100)),'a') as file:
+                file.write(f"{xpoint},{1.0 - train_acc},{loss.item()}" + "\n")
+            
             # if batich_idx % 100 == 0 and batich_idx != 0:
             #     stdout_temp = 'batch: {:>3}/{:<3}, train acc:{:<8}, train loss: {:<8}'
             #     print(stdout_temp.format(batich_idx, len(train_loader), train_acc, loss.item()))
-            
-        test_acc, test_loss = test(model, device, test_loader, criterion)
 
-        teerr.append(1.0 - test_acc)
-        teloss.append(test_loss)
-        test_data_save.append([epoch + 1, 1.0 - test_acc, test_loss])
+        ## これないとダメmodel.eval
+        model.eval()
+        test_acc, test_loss = test(model, device, test_loader, criterion)
+        # csv writer
+        with open('./csv/{}_{}_{}epochs_ln{}pc_test.csv'.format(args.model_fullname, args.dataset, args.epoch, int(label_noise_rate * 100)),'a') as file:
+                file.write(f"{epoch + 1},{1.0 - test_acc},{test_loss}" + "\n")
+
         # Output score.
         stdout_temp = 'epoch: {:>3}, train acc: {:<8}, train loss: {:<8}, test acc: {:<8}, test loss: {:<8}'
         print(stdout_temp.format(epoch+1, train_acc, loss.item(), test_acc, test_loss))
+
+        ## model save###
+        # torch.save(model.state_dict(), './model_weight/resnet18*' + str(args.model_width) + '-cifar10-train.csv')
+        # pytorchの慣例でpthファイルで保存する
+        # 実験のためにepochごとに保存
+        model_path = f'./model_weight/{args.model_fullname}_epoch{(epoch + 1):04}_{args.dataset}_ln{int(label_noise_rate * 100)}pc.pth'
+        torch.save(model.state_dict(), model_path)
     
-    torch.save(model.state_dict(), './model_weight/resnet18*' + str(args.model_width) + '-cifar10-train.csv')
-
-    with open('./csv/resnet18*' + str(args.model_width) + '-cifar10-train.csv','w') as file:
-        writer = csv.writer(file)
-        writer.writerows(train_data_save)
-
-    with open('./csv/resnet18*' + str(args.model_width) + '-cifar10-test.csv','w') as file:
-        writer = csv.writer(file)
-        writer.writerows(test_data_save)
+    # checkpoint
+    torch.save({
+    "epoch": epoch,
+    "model_state_dict": model.state_dict(),
+    "optimizer_state_dict": optimizer.state_dict()
+    }, 
+    f'./checkpoint/{args.model_fullname}_{(epoch + 1)}epochs_{args.dataset}_ln{int(label_noise_rate * 100)}pc.tar')
     
-    #errorのグラフ化
-    plt.title("ResNet18*" + str(args.model_width) + " trained by Cifar10")
-    plt.xlim(0, epoch * 1.2)
-    plt.ylim(0, 1)
-    plt.xlabel("Epoch")
-    plt.ylabel("Erorr")
-    plt.plot(x1, trerr, label='train', linewidth=0.5)
-    plt.plot(x2, teerr, label='test')
-    plt.legend(loc='upper right')
-    plt.savefig("./output/ResNet18*" + str(args.model_width) + "TrainedByCifar10.png")
-
-    plt.close()
-
-    #lossのグラフ化
-    plt.title("ResNet18*" + str(args.model_width) + " trained by Cifar10")
-    plt.xlim(0, epoch * 1.2)
-    plt.ylim(0, max(max(trloss) * 1.2, max(teloss) * 1.2))
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.plot(x1, trloss, label='train', linewidth=0.5)
-    plt.plot(x2, teloss, label='test')
-    plt.legend(loc='upper right')
-    plt.savefig("./output/loss-ResNet18*" + str(args.model_width) + "TrainedByCifar10.png")
-
     
 
 def train (model, device, train_loader, criterion, optimizer):
@@ -339,9 +283,15 @@ def calc_score(true_list, predict_list, running_loss, data_loader):
 def parse_args():
     arg_parser = argparse.ArgumentParser(description="ResNet trained by CIFAR-10")
     
-    arg_parser.add_argument("-k", "--model_width", nargs="?", type=int, default=1)
-    arg_parser.add_argument("grad_steps", nargs="?", type=int, default=50000)
-    arg_parser.add_argument("label_noise_rate", nargs="?", type=float, default=0.0)
+    arg_parser.add_argument("-k", "--model_width", type=int, default=1)
+    arg_parser.add_argument("-e", "--epoch", type=int, default=4000)
+    arg_parser.add_argument("-l", "--label_noise_rate", type=float, default=0.0)
+
+    # 追加
+    arg_parser.add_argument("--model", type=str, choices=["resnet18k", "resnet18", "resnet50"], help="モデルアーキテクチャの選択")
+    arg_parser.add_argument("-pt", "--pretrained", action='store_true', help="事前学習モデルの利用．ResNet18のときのみ有効．引数をつけるとTrue")
+    arg_parser.add_argument("-ds", "--dataset", type=str, choices=["cifar10", "cifar100", "tinyImageNet"], default="cifar10")
+    arg_parser.add_argument("--num_classes", type=int, default=10, help="分類クラス数")
 
     return arg_parser.parse_args()
 
@@ -349,5 +299,6 @@ if __name__ =='__main__':
     warnings.filterwarnings('ignore')
     import time
     start = time.perf_counter()
-    sub()
+    main()
+    
     print(time.perf_counter() - start)
